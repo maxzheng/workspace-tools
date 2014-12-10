39a00c08f4b92b617248703b5b6490950b69e1bd
Max Zheng <mzheng@linkedin.com>
2014-12-09 19:11:32 -0800

    Initial setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2.6
+
+import os
+import setuptools
+
+
+def find_files(path):
+  return [os.path.join(path, f) for f in os.listdir(path)]
+
+
+setuptools.setup(
+  name = 'workspace-tools',
+  version = '0.0.1',
+
+  author = 'Max Zheng',
+  author_email = 'mzheng@linkedin.com',
+
+  description = open('README.md').read(),
+
+  install_requires=[
+    'brownie',
+  ],
+
+  setup_requires=['setuptools-git'],
+
+  package_dir={'': 'src'},
+  packages=setuptools.find_packages('src'),
+  include_package_data=True,
+
+  scripts=find_files('bin'),
+)
