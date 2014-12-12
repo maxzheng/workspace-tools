9ac78dc5ac0829a16dbe7724fe4578835b905934
Max Zheng <mzheng@linkedin.com>
2014-12-12 06:15:16 -0800

    Clean out build/dist files

--- src/workspace/commands/clean.py
+++ src/workspace/commands/clean.py
@@ -14,9 +14,14 @@ def setup_clean_parser(subparsers):
 
 
 def clean(**kwargs):
-  """ Clean workspace by removing .pyc files """
+  """ Clean workspace by removing build, dist, and .pyc files """
 
   path = workspace_path()
   log.info('Cleaning workspace %s', path)
 
-  silent_run("find . -path */.tox -prune -o -path */mppy-* -prune -o -name *.pyc -delete", cwd=path)
+  log.info('Removing build/dist folders')
+  silent_run("rm -rf */build */dist", cwd=path, shell=True)
+
+  log.info('Removing *.pyc files')
+  silent_run("find . -path /*/.tox -prune -o -path /*/mppy-* -prune -o -name *.pyc -delete", cwd=path)
+
