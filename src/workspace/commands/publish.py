291b8111b70ae9e5eba6d3448add97f9fb4b570f
Max Zheng <mzheng@linkedin.com>
2014-12-16 08:02:22 -0800

    Change text for bump/upload in publish

--- src/workspace/commands/publish.py
+++ src/workspace/commands/publish.py
@@ -34,12 +34,12 @@ def publish(minor=False, major=False, **kwargs):
     update(raises=True)
 
     new_version = bump_version(major, minor)
-    commit(msg='Bump version to ' + new_version, push=True)
+    commit(msg='Release version ' + new_version, push=True)
 
     log.info('Building source distribution')
     silent_run('python setup.py sdist')
 
-    log.info('Uploading with twine')
+    log.info('Uploading')
     silent_run('twine upload dist/*', shell=True)
 
 
