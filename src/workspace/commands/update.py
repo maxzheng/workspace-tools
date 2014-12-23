11a869592855c825df0d9712adbd8e3663463e43
Max Zheng <mzheng@linkedin.com>
2014-12-23 15:26:08 -0800

    Add product/group filter option for update

--- src/workspace/commands/update.py
+++ src/workspace/commands/update.py
@@ -1,5 +1,6 @@
 import logging
 
+from workspace.commands.helpers import expand_product_groups
 from workspace.scm import is_git_repo, checkout_branch, update_repo, repos, product_name_for_repo, current_branch,\
     update_branch
 
@@ -8,16 +9,26 @@ log = logging.getLogger(__name__)
 
 def setup_update_parser(subparsers):
   update_parser = subparsers.add_parser('update', aliases=['up'], description=update.__doc__, help=update.__doc__)
+  update_parser.add_argument('products', nargs='*', help='When updating all products, filter by these products or product groups')
   update_parser.set_defaults(command=update)
 
   return update_parser
 
 
-def update(raises=False, **kwargs):
+def update(products=None, raises=False, **kwargs):
   """ Update current product or all products in workspace """
 
+  if products:
+    products = expand_product_groups(products)
+
   for repo in repos():
-    log.info('Updating %s', product_name_for_repo(repo))
+    name = product_name_for_repo(repo)
+
+    if products and name not in products:
+      continue
+
+    log.info('Updating %s', name)
+
     try:
       branch = None
       if is_git_repo(repo):
