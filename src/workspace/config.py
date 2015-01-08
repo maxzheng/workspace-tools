00df19bb0e6fefce5b62cafbc311ce56f8b17a5c
Max Zheng <mzheng@linkedin.com>
2015-01-08 03:04:25 -0800

    Update config comments

--- src/workspace/config.py
+++ src/workspace/config.py
@@ -1,24 +1,24 @@
 """
-# Default config. Change these in your personal ~/.config/workspace.cfg
+# Default config. Change these in your personal ~/.config/workspace.cfg ::
 
-# Define product groups to take action upon (such as ws develop)
-[product_groups]
-#group_name = product_checkout1 lib_checkout2
-
-
-# Settings for checkout command
-[checkout]
-
-# Check out SVN repo using git-svn and clone the specified # of commits. Set to 0 to disable git-svn clone.
-# Higher the number, the longer it takes to clone.
-use_gitsvn_to_clone_svn_commits = 10
+  # Define product groups to take action upon (such as ws develop)
+  [product_groups]
+  #group_name = product_checkout1 lib_checkout2
 
 
-# Settings for bump command
-[bump]
+  # Settings for checkout command
+  [checkout]
 
-# List of requirement files to check / bump dependencies in
-requirement_files = requirements.txt pinned.txt
+  # Check out SVN repo using git-svn and clone the specified # of commits. Set to 0 to disable git-svn clone.
+  # Higher the number, the longer it takes to clone.
+  use_gitsvn_to_clone_svn_commits = 10
+
+
+  # Settings for bump command
+  [bump]
+
+  # List of requirement files to check / bump dependencies in
+  requirement_files = requirements.txt pinned.txt
 """
 
 import logging
@@ -34,7 +34,7 @@ USER_CONFIG_FILE = os.path.join('~', '.config', CONFIG_FILE)
 
 log = logging.getLogger()
 config = RemoteConfig(USER_CONFIG_FILE)
-config.read(__doc__)
+config.read(__doc__.replace('\n  ', '\n'))
 
 
 def product_groups():
