0968c2268f79e76e9cf40f6d8c1350376424f520
Max Zheng <mzheng@confluent.io>
2017-07-20 14:04:06 -0700

    Support multiple repositories in publish

--- workspace/commands/publish.py
+++ workspace/commands/publish.py
@@ -22,16 +22,18 @@ IGNORE_CHANGE_RE = re.compile('^(?:Update changelog|Fix tests?)\s*$', flags=re.I
 
 class Publish(AbstractCommand):
     """
-      Bumps version in setup.py (defaults to patch), writes out changelog, builds a source distribution,
-      and uploads with twine.
+        Bumps version in setup.py (defaults to patch), writes out changelog, builds a source distribution,
+        and uploads with twine.
 
-      :param bool minor: Perform a minor publish by bumping the minor version
-      :param bool major: Perform a major publish by bumping the major version
+        :param str repo: Repository to publish to
+        :param bool minor: Perform a minor publish by bumping the minor version
+        :param bool major: Perform a major publish by bumping the major version
     """
     @classmethod
     def arguments(cls):
         _, docs = cls.docs()
         return [
+          cls.make_args('-r', '--repo', default='pypi', help=docs['repo']),
           cls.make_args('--minor', action='store_true', help=docs['minor']),
           cls.make_args('--major', action='store_true', help=docs['major'])
         ]
@@ -44,18 +46,23 @@ class Publish(AbstractCommand):
         repo_check()
 
         pypirc = LocalConfig('~/.pypirc')
-        if not pypirc.pypi or not pypirc.pypi.repository:
-            log.error('Please add repository / username to [pypi] section in ~/.pypirc')
+        repository = pypirc.get(self.repo, 'repository')
+        username = pypirc.get(self.repo, 'username')
+        password = pypirc.get(self.repo, 'password')
+        repo_title = ('PyPI' if self.repo == 'pypi' else self.repo).capitalize()
+
+        if not repository:
+            log.error('Please add repository / username to [%s] section in ~/.pypirc', self.repo)
             sys.exit(1)
 
-        if not pypirc.pypi.username:
-            pypirc.pypi.username = getpass.getuser('PyPI Username: ')
-            if not pypirc.pypi.username:
+        if not username:
+            username = getpass.getuser('{} Username: '.format(repo_title))
+            if not username:
                 sys.exit()
 
-        if not pypirc.pypi.password:
-            pypirc.pypi.password = getpass.getpass('PyPI Password: ')
-            if not pypirc.pypi.password:
+        if not password:
+            password = getpass.getpass('{} Password: '.format(repo_title))
+            if not password:
                 sys.exit()
 
         self.commander.run('update')
@@ -71,15 +78,17 @@ class Publish(AbstractCommand):
         new_version = self.bump_version()
         self.update_changelog(new_version, changes, self.minor or self.major)
 
-        self.commander.run('commit', msg=PUBLISH_VERSION_PREFIX + new_version, push=True)
+        self.commander.run('commit', msg=PUBLISH_VERSION_PREFIX + new_version, push=2)
 
         click.echo('Building source distribution')
         silent_run('python setup.py sdist', cwd=repo_path())
 
-        click.echo('Uploading')
+        click.echo('Uploading to ' + repo_title)
 
-        silent_run('twine upload -u "{username}" -p "{password}" dist/*'.format(
-            **dict(list(pypirc.pypi))), shell=True, cwd=repo_path())
+        silent_run('twine upload -r "{repo}" -u "{username}" -p "{password}" dist/*'.format(
+            repo=self.repo,
+            username=username,
+            password=password), shell=True, cwd=repo_path())
 
     def changes_since_last_publish(self):
         commit_msgs = extract_commit_msgs(commit_logs(limit=100, repo=repo_path()), True)
