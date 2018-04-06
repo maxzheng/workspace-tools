fd4d90939c733536afc394b7f4db7655a7894cc7
Max Zheng <maxzheng.os@gmail.com>
2018-04-06 18:16:31 +0000

    Set min version to Python 3.6

--- setup.py
+++ setup.py
@@ -9,45 +9,44 @@ def find_files(path):
 
 
 setuptools.setup(
-  name='workspace-tools',
-  version='3.1.0',
+    name='workspace-tools',
+    version='3.1.0',
 
-  author='Max Zheng',
-  author_email='maxzheng.os@gmail.com',
+    author='Max Zheng',
+    author_email='maxzheng.os@gmail.com',
 
-  description='Convenience wrapper for git/tox to simplify local development',
-  long_description=open('README.rst').read(),
+    description='Convenience wrapper for git/tox to simplify local development',
+    long_description=open('README.rst').read(),
 
-  changelog_url='https://raw.githubusercontent.com/maxzheng/workspace-tools/master/docs/CHANGELOG.rst',
-  url='https://github.com/maxzheng/workspace-tools',
+    changelog_url='https://raw.githubusercontent.com/maxzheng/workspace-tools/master/docs/CHANGELOG.rst',
+    url='https://github.com/maxzheng/workspace-tools',
 
-  entry_points={
-    'console_scripts': [
-      'wst = workspace.controller:Commander.main',
+    entry_points={
+        'console_scripts': [
+            'wst = workspace.controller:Commander.main',
+        ],
+    },
+
+    install_requires=open('requirements.txt').read(),
+
+    license='MIT',
+
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+
+    python_requires='>=3.6',
+    setup_requires=['setuptools-git'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
     ],
-  },
 
-  install_requires=open('requirements.txt').read(),
-
-  license='MIT',
-
-  packages=setuptools.find_packages(),
-  include_package_data=True,
-
-  setup_requires=['setuptools-git'],
-
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-
-    'Intended Audience :: Developers',
-    'Topic :: Software Development',
-
-    'License :: OSI Approved :: MIT License',
-
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-  ],
-
-  keywords='workspace multiple repositories git tox wrapper development tools',
+    keywords='workspace multiple repositories git tox wrapper development tools',
 )
