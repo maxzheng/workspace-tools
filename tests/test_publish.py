e7c9d008e517f45906153eb1d5da03df45e35d2a
Max Zheng <maxzheng.os@gmail.com>
2018-04-19 05:48:49 +0000

    Add test for publish

---
+++ tests/test_publish.py
@@ -0,0 +1,25 @@
+from mock import Mock, call
+
+from workspace.utils import run
+
+from test_stubs import temp_git_repo
+
+
+def test_status(wst, monkeypatch):
+    silent_run_mock = Mock()
+    config_mock = Mock()
+    monkeypatch.setattr('workspace.commands.publish.LocalConfig', config_mock)
+    monkeypatch.setattr('workspace.commands.publish.silent_run', silent_run_mock)
+
+    config_mock().get.side_effect = ['repo', 'user', 'pass']
+
+    with temp_git_repo() as cwd:
+        wst('setup --product')
+        run('git commit --allow-empty -m dummy-commit')
+
+        wst('publish')
+
+    assert silent_run_mock.call_args_list == [
+        call('rm -rf dist/*', cwd=str(cwd), shell=True),
+        call('python setup.py sdist', cwd=str(cwd)),
+        call('twine upload -r "pypi" -u "user" -p "pass" dist/*', cwd=str(cwd), shell=True)]
