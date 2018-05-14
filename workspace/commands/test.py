4265d2f6ded4ba6e785e31575babca413b80a376
Max Zheng <maxzheng.os@gmail.com>
2018-05-14 21:19:03 +0000

    Add --text option for test coverage

--- workspace/commands/test.py
+++ workspace/commands/test.py
@@ -58,6 +58,7 @@ class Test(AbstractCommand):
       :param bool debug: Turn on debug logging
       :param list install_editable: List of products or product groups to install in editable mode.
       :param list extra_args: Extra args from argparse to be passed to pytest
+      :param bool text: Add "--cov-report=annotate:textcov" to pytest args.
       :return: Dict of env to commands ran on success. If return_output is True, return a string output.
                If test_dependents is True, return a mapping of product name to the mentioned results.
     """
@@ -79,7 +80,8 @@ class Test(AbstractCommand):
           cls.make_args('-t', '--test-dependents', action='store_true', help=docs['test_dependents']),
           cls.make_args('-r', '--redevelop', action='count', help=docs['redevelop']),
           cls.make_args('-o', action='store_true', dest='install_only', help=argparse.SUPPRESS),
-          cls.make_args('-e', '--install-editable', nargs='+', help=docs['install_editable'])
+          cls.make_args('-e', '--install-editable', nargs='+', help=docs['install_editable']),
+          cls.make_args('--text', action='store_true', help=docs['text'])
         ]
 
     @classmethod
@@ -228,12 +230,14 @@ class Test(AbstractCommand):
                     envs.append(ef)
 
         pytest_args = ''
-        if self.match_test or self.num_processes is not None or files or self.extra_args:
+        if self.match_test or self.num_processes is not None or files or self.extra_args or self.text:
             pytest_args = []
             if self.match_test:
                 pytest_args.append('-k ' + self.match_test)
             if self.num_processes is not None:
                 pytest_args.append('-n ' + str(self.num_processes))
+            if self.text:
+                pytest_args.append('--cov-report=annotate:textcov')
             if self.extra_args:
                 pytest_args.extend(self.extra_args)
             if files:
