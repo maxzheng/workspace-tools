06d44427876c0708eef15b4194dcedab3a8cd55f
Max Zheng <maxzheng.os@gmail.com>
2019-10-08 01:51:53 +0000

    Publish version 3.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def find_files(path):
 
 setuptools.setup(
     name='workspace-tools',
-    version='3.4.0',
+    version='3.4.1',
 
     author='Max Zheng',
     author_email='maxzheng.os@gmail.com',
